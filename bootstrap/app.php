fdb976e2a4412296c07f492581380eec9e7eb6fe
dxseless <dm739165@gmail.com>
2025-04-01 22:18:50 +0300

    fixed specific ticket data request

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::prefix('api/v1')->group(function () {
+            Route::middleware('api')->prefix('api/v1')->group(function () {
                 require __DIR__.'/../routes/api_v1.php';
             });
         }
